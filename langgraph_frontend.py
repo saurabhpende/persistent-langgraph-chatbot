b2637ba97e77c2bb9f2ccabff04419d0281ca2a4
saurabhpende <87076250+saurabhpende@users.noreply.github.com>
2025-08-14 20:32:56 +0530

    Update langgraph_frontend.py

--- langgraph_frontend.py
+++ langgraph_frontend.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langgraph_backend import chatbot
+from langgraph_database_backend import chatbot,retrive_all_threads
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
 import uuid
@@ -49,7 +49,7 @@ if 'thread_id' not in st.session_state:
     st.session_state['thread_id'] = generate_thread_id()
 
 if 'chat_threads' not in st.session_state:
-    st.session_state['chat_threads'] = []
+    st.session_state['chat_threads'] = retrive_all_threads()
 
 if 'chat_titles' not in st.session_state:
     st.session_state['chat_titles'] = {}
